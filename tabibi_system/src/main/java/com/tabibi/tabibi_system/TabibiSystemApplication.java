c07bec0a6ce9ce982c89634e45bfc2273d31d878
arafat3300 <125751269+arafat3300@users.noreply.github.com>
2024-02-26 11:19:00 +0200

    Spring boot setup

---
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/TabibiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tabibi.tabibi_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabibiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabibiSystemApplication.class, args);
+	}
+
+}
---
+++ tabibi_system/src/test/java/com/tabibi/tabibi_system/TabibiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabibi.tabibi_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabibiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
