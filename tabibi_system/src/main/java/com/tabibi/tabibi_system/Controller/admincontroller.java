2fa2caa9f578564c3fb09df6569b360ebce5c593
Samirzzz <abdelrahman.200300@gmail.com>
2024-03-03 05:05:12 +0200

    admin css+css bug

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Controller/admincontroller.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Controller/admincontroller.java
@@ -1,4 +1,3 @@
-
 package com.tabibi.tabibi_system.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,39 +14,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
 @RestController
 @RequestMapping("/User")
-public class UserController {
-    @Autowired
-    private UserRepository UserRepository;
-
-
-
-    @GetMapping("Registration")
-
-    public ModelAndView addUser()
-    {
-    ModelAndView mav=new ModelAndView("registration.html");
-    User newUser=new User();
-    mav.addObject("User", newUser);
-      return mav;
-    }
-
-    @PostMapping("Registration")
-    public String saveUser(@ModelAttribute User user)
-     {
-      String encoddedPassword=BCrypt.hashpw(user.getPassword(), BCrypt.gensalt(12)) ;
-      user.setPassword(encoddedPassword);
-      this.UserRepository.save(user);
-      return "ADDed ya basha to DataBAse";
-     }
-   
-   @GetMapping("/navigation")
-   public ModelAndView getnavigation() {
-    ModelAndView mav=new ModelAndView("navigation.html");
+public class admincontroller {
+    @GetMapping("/admin-dashboard")
+   public ModelAndView getadmin_dashboard() {
+    ModelAndView mav=new ModelAndView("admindashboard.html");
        return mav;
    }
-     
-    
+   @GetMapping("/User")
+   public String getpage() {
+       return new String();
+   }
+   
 }
---
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Model/usertype.java
@@ -0,0 +1,9 @@
+package com.tabibi.tabibi_system.Model;
+
+public class usertype {
+ 
+
+    public String utid;
+    public String name;
+    public String pages;
+}
