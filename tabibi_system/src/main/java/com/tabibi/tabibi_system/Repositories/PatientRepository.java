9e146e618a6a3f1b339dfeef2e0040ef53564825
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-03-17 05:30:06 +0200

    create the query in the repository

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/PatientRepository.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/PatientRepository.java
@@ -1,7 +1,9 @@
 package com.tabibi.tabibi_system.Repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tabibi.tabibi_system.Models.Patient;
 import com.tabibi.tabibi_system.Models.UserAcc;
@@ -11,5 +13,8 @@ import com.tabibi.tabibi_system.Models.UserAcc;
 public interface PatientRepository extends JpaRepository<Patient,Long> 
 {
     Patient  findByUserAcc(UserAcc userAcc);
+
+    @Query("SELECT * FROM patient e WHERE WHERE CONCAT(firstName, ' ', lastName) LIKE %:substring%")
+    List<Patient> findBySubstring(String substring);
     
 }
