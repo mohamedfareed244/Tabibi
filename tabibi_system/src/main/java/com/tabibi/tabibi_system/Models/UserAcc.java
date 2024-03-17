198f09be3e3b1bd37b03360fd37941d0ec4fffa5
arafat3300 <125751269+arafat3300@users.noreply.github.com>
2024-03-17 16:02:42 +0200

    signup bug

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Models/UserAcc.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Models/UserAcc.java
@@ -2,6 +2,7 @@ package com.tabibi.tabibi_system.Models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Objects;
@@ -17,7 +18,7 @@ public class UserAcc {
     private int uid;
 
     @Email
-    @NotBlank
+    @NotEmpty(message = "email cannot be empty ")
     @Column(name = "email", nullable = false)
     private String email;
     
