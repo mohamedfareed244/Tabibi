a755f648224fc46e18b7586ef765596078b63025
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-09 01:14:31 +0300

    create the pointcut

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
@@ -1,5 +1,14 @@
 package com.tabibi.tabibi_system.Logging;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class userlogging {
-    
+    @Pointcut("execution ( com.tabibi.tabibi_system.Controllers.UserController.loginprocess(..))")
+    public void UserLoginPointCut(){
+        
+    }
 }
