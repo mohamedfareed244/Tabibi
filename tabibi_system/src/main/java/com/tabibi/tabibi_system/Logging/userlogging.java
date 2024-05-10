c91c98d677fdb0a50492c18d6c1cb7a12d879628
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-10 03:03:09 +0300

    start in the logging

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
@@ -1,9 +1,16 @@
 package com.tabibi.tabibi_system.Logging;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.tabibi.tabibi_system.Repositories.UserRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Aspect
 @Component
@@ -12,8 +19,17 @@ public class userlogging {
     public void UserLoginPointCut(){
 
     }
-    @After(value ="UserLoginPointCut()")
-    public void UserLoginLogger(){
-        
+    @AfterReturning(
+        pointcut = "execution(* your.package.loginprocess(..)) && args(email, pass, session)",
+        returning = "redirectView")
+public void logSuccessfulLogin(JoinPoint joinPoint, RedirectView redirectView, String email, String pass, HttpSession session) {
+    // Check if redirection is to DoctorHomePage
+    if (redirectView != null && "/User/DoctorHomePage".equals(redirectView.getUrl())) {
+        // Extract user ID
+        String userId = (String) session.getAttribute("uid");
+        // Log the successful login event with user ID
+        // You can use your preferred logging mechanism here
+        System.out.println("User with ID " + userId + " signed in and redirected to DoctorHomePage at: " + java.time.LocalDateTime.now());
     }
+}
 }
--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/UserLogRepository.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/UserLogRepository.java
@@ -1,5 +1,9 @@
 package com.tabibi.tabibi_system.Repositories;
 
-public class UserLogRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tabibi.tabibi_system.Models.UserLog;
+
+public interface UserLogRepository extends JpaRepository<UserLog,Integer> {
+  
 }
