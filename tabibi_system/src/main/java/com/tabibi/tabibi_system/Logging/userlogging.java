6e8d0c66fe379614630144623d5656326acd3b88
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-16 20:11:02 +0300

    user signin log done

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -19,6 +20,9 @@ import jakarta.servlet.http.HttpSession;
 @Aspect
 @Component
 public class userlogging {
+    @Autowired
+    UserLogRepository userlog;
+
     @Pointcut("execution(* com.tabibi.tabibi_system.Controllers.UserController.loginprocess(..))")
     public void UserLoginPointCut(){
 
@@ -29,15 +33,19 @@ public class userlogging {
         public void logSuccessfulLogin(JoinPoint joinPoint, RedirectView redirectView) {
     // Check if redirection is to DoctorHomePage
     if (redirectView != null && ( "/User/DoctorHomePage".equals(redirectView.getUrl()) || "/User/clinicHomepage".equals(redirectView.getUrl()) || "/User/patientHomepage".equals(redirectView.getUrl()) ) ) {
-       
-        // String userId = (String) session.getAttribute("uid");
+
+        Object[] args = joinPoint.getArgs();
+        HttpSession session = (HttpSession) args[2];
+        int userId = (int) session.getAttribute("uid");
      
-    //    UserLogRepository userlog;
-    //    Date currentDate = new Date(System.currentTimeMillis());
-    //    UserLog NewLog=new UserLog();
-    Object[] args = joinPoint.getArgs();
-    String email = (String) args[0];
-    System.out.println("login catched "+email);
+       
+       Date currentDate = new Date(System.currentTimeMillis());
+       UserLog NewLog=new UserLog();
+   NewLog.setDate(currentDate);
+   NewLog.setUserId(userId);
+    userlog.save(NewLog);
+
+   
     }
  
 }
