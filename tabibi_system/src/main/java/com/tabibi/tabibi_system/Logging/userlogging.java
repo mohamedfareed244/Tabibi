275513fad6ab13a92eda05e253212cb4af6decf7
mohamedfareed244 <mohamed2102759@miuegypt.edu.eg>
2024-05-16 19:43:46 +0300

    aop logging basics

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Logging/userlogging.java
@@ -1,40 +1,41 @@
-// package com.tabibi.tabibi_system.Logging;
+package com.tabibi.tabibi_system.Logging;
 
-// import java.sql.Date;
+import java.sql.Date;
 
-// import org.aspectj.lang.JoinPoint;
-// import org.aspectj.lang.annotation.After;
-// import org.aspectj.lang.annotation.AfterReturning;
-// import org.aspectj.lang.annotation.Aspect;
-// import org.aspectj.lang.annotation.Pointcut;
-// import org.springframework.stereotype.Component;
-// import org.springframework.web.servlet.view.RedirectView;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.RedirectView;
 
-// import com.tabibi.tabibi_system.Models.UserLog;
-// import com.tabibi.tabibi_system.Repositories.UserLogRepository;
-// import com.tabibi.tabibi_system.Repositories.UserRepository;
+import com.tabibi.tabibi_system.Models.UserLog;
+import com.tabibi.tabibi_system.Repositories.UserLogRepository;
+import com.tabibi.tabibi_system.Repositories.UserRepository;
 
-// import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 
-// @Aspect
-// @Component
-// public class userlogging {
-//     @Pointcut("execution ( com.tabibi.tabibi_system.Controllers.UserController.loginprocess(..))")
-//     public void UserLoginPointCut(){
+@Aspect
+@Component
+public class userlogging {
+    @Pointcut("execution(* com.tabibi.tabibi_system.Controllers.UserController.loginprocess(..))")
+    public void UserLoginPointCut(){
 
-//     }
-//     @AfterReturning(
-//         pointcut = "UserLoginPointCut()",
-//         returning = "redirectView")
-// public void logSuccessfulLogin(JoinPoint joinPoint, RedirectView redirectView, String email, String pass, HttpSession session) {
-//     // Check if redirection is to DoctorHomePage
-//     if (redirectView != null && ( "/User/DoctorHomePage".equals(redirectView.getUrl()) || "/User/clinicHomepage".equals(redirectView.getUrl()) || "/User/patientHomepage".equals(redirectView.getUrl()) ) ) {
-//         // Extract user ID
-//         String userId = (String) session.getAttribute("uid");
-//         // Log the successful login event with user ID
-//        UserLogRepository userlog;
-//        Date currentDate = new Date(System.currentTimeMillis());
-//        UserLog NewLog=new UserLog();
-//     }
-// }
-// }
+    }
+    @AfterReturning(
+        pointcut = "UserLoginPointCut()",
+        returning = "redirectView")
+        public void logSuccessfulLogin(JoinPoint joinPoint, RedirectView redirectView) {
+    // Check if redirection is to DoctorHomePage
+    // if (redirectView != null && ( "/User/DoctorHomePage".equals(redirectView.getUrl()) || "/User/clinicHomepage".equals(redirectView.getUrl()) || "/User/patientHomepage".equals(redirectView.getUrl()) ) ) {
+    //     // Extract user ID
+    //     String userId = (String) session.getAttribute("uid");
+    //     // Log the successful login event with user ID
+    //    UserLogRepository userlog;
+    //    Date currentDate = new Date(System.currentTimeMillis());
+    //    UserLog NewLog=new UserLog();
+    // }
+    System.out.println("login catched ");
+}
+}
