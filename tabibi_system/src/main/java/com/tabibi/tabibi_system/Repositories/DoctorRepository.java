b530a1cae8f9f924d29f4b9eac3fb45b9799e686
seif2102995 <seif2102995@miuegypt.edu.eg>
2024-05-19 02:38:59 +0300

    dynamic searcg and filtering to help the patient book the appointment

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/DoctorRepository.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/DoctorRepository.java
@@ -3,6 +3,7 @@ package com.tabibi.tabibi_system.Repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tabibi.tabibi_system.Models.Doctor;
 import com.tabibi.tabibi_system.Models.Patient;
@@ -14,6 +15,8 @@ public interface DoctorRepository extends JpaRepository<Doctor,Integer>
           //   Doctor  findByUserAcc(UserAcc userAcc);
    Doctor findByEmail(String email);
    Doctor findByUid(int uid);
+    @Query("SELECT DISTINCT d.specialization FROM Appointment a JOIN a.doctor d")
+    List<String> findDistinctSpecializationsByAppointments();
 
 
 }
