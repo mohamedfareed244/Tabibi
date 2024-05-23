2b9f4004470cd540c26eb5ee0d0f576888ea7e6d
seif2102995 <seif2102995@miuegypt.edu.eg>
2024-05-23 20:01:29 +0300

    workplaces repository methods

--- tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/WorkplacesRepository.java
+++ tabibi_system/src/main/java/com/tabibi/tabibi_system/Repositories/WorkplacesRepository.java
@@ -1,10 +1,15 @@
 package com.tabibi.tabibi_system.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.tabibi.tabibi_system.Models.Clinic;
+import com.tabibi.tabibi_system.Models.Doctor;
 import com.tabibi.tabibi_system.Models.Workplaces;
 
 public interface WorkplacesRepository extends JpaRepository<Workplaces,Long> 
 {
-    
+  List<Workplaces> findByClinic(Clinic clinic);
+ boolean existsByDoctorAndClinic(Doctor doctor, Clinic clinic);   
 }
